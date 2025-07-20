25824ea3c8f561c406f53252163ef818f019de9e
Diana <bu_di_2005@mail.ru>
2025-07-20 20:03:32 +0300

    db

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,11 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 export const connectDB = async () => {
